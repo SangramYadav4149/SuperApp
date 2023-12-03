9ca22039a6c12ab081a95825704edc7ce0c302c2
SangramYadav4149 <sangramy114@gmail.com>
2023-12-03 17:29:29 +0530

    added user profile card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Category from "./Pages/MainCategory/MainCategory";
+import Info from "./Components/Browse/Info";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Info />} />
         <Route path="/category" element={<Category />} />
       </Routes>
     </BrowserRouter>
---
+++ src/Components/Browse/Info.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Info.css";
+import img from "../../Assets/profileBig.png";
+const info = () => {
+  const userInfo = localStorage.getItem("userInfo");
+  return (
+    <section className="info-wrapper">
+      <div className="info-container">
+        <div className="img">
+          <img src={img}></img>
+        </div>
+        <div className="user-info">
+          <div className="container">
+            <span className="name">Rishieksh ghosh</span>
+            <span className="email">rishikeshghosh@gmail.com</span>
+            <span className="userName">Rishikeh</span>
+          </div>
+          <div className="container-cate">
+            <span className="cate">Horror</span>
+            <span className="cate">Thirler</span>
+            <span className="cate">Action</span>
+            <span className="cate">Fiction</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default info;
